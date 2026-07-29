387e8558e29ed88bc72f25044f58e0784600fcb0
antonboshoff67-tech <anton.boshoff67@gmail.com>
2026-07-29 16:52:41 -0500

    Cleaned build artifacts and updated .gitignore

--- target/classes/Start_and_Stop/Stop-Item-Kafka-Producer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#pid=$( ps -ef|grep java|grep Leads-Service|awk {'print $2'} );
-#if [ "$pid" != "" ];
-#    then kill $pid;
-#fi;
-#!/bin/bash
-
-kill -9 `ps -ef | grep java | grep EF-Hello_World-Agent-POC | awk {'print $2'}`
